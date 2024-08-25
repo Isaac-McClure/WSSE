db01fe2247f7bbfedb5a7dab6af9e6b29b1b54fd
Isaac-McClure <isaac.mcclure.3@gmail.com>
2024-08-26 09:01:03 +1000

    Created a console project, added google sheets library, configured appsettings to provide api key.

---
+++ WSSEGenerator/Models/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace WSSEGenerator.Models
+{
+    public sealed class Settings
+    {
+        public string GoogleApiKey { get; set; } = "";
+    }
+}
---
+++ WSSEGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using WSSEGenerator.Models;
+
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration.Sources.Clear();
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+Settings settings = builder.Configuration.Get<Settings>();
+
+using IHost host = builder.Build();
+
+if (settings == null)
+{
+    throw new Exception("Could not bind configuration. Ensure appsettings.json is present and correct.");
+}
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"API Key: {settings.GoogleApiKey}");
+
+await host.RunAsync();
+
